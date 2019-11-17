d8acf49a220311f70cdb875d9443ab91943d0d8c
Yasar Yousuf <yousuf802@gmail.com>
2019-11-17 23:17:29 +0600

    local commit

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,23 @@ Auth::routes();
 
 Route::get('/contact', 'HomeController@contact');
 
-Route::get('/user/edit', 'UserController@edit');
-Route::get('/user/dashboard', 'UserController@dashboard');
-Route::get('/user/questions', 'UserController@questions');
 
+Route::prefix('user')->group(function(){
+    Route::get('/edit', 'UserController@edit');
+    Route::get('/dashboard', 'UserController@dashboard');
+    Route::get('/questions', 'UserController@questions');
+});
 
-Route::get('/questions', 'QuestionController@index');
-Route::get('/question/ask', 'QuestionController@create')->middleware('auth');
-Route::get('/question/{question}', 'QuestionController@show');
-Route::post('/question', 'QuestionController@store')->name('question.store');
-Route::get('/question/{question}/edit', 'QuestionController@edit')->name('questions.edit')->middleware('auth');
-Route::put('/question/{question}', 'QuestionController@update')->name('questions.update')->middleware('auth');
-Route::delete('/question/{question}', 'QuestionController@destroy')->name('questions.destroy')->middleware('auth');
+Route::prefix('questions')->group(function () {
+    Route::get('/', 'QuestionController@index');
+    Route::get('/ask', 'QuestionController@create')->middleware('auth');
+    Route::get('/{question}', 'QuestionController@show');
+    Route::post('/', 'QuestionController@store')->name('question.store');
+    Route::get('/{question}/edit', 'QuestionController@edit')->name('questions.edit')->middleware('auth');
+    Route::put('/{question}', 'QuestionController@update')->name('questions.update')->middleware('auth');
+    Route::delete('/{question}', 'QuestionController@destroy')->name('questions.destroy')->middleware('auth');
+    Route::post('/{id}/comment', 'CommentController@store');
+});
 
-Route::post('/question/{id}/comment', 'CommentController@store');
 
 Route::get('/tag/{tag}', 'TagController@show');
